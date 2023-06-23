af6067aff33919ad5e523a9ba3764f7e19d4dbdd
daxdiv <95906740+daxdiv@users.noreply.github.com>
2023-06-23 17:45:38 +0200

    refactor

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,41 +1,22 @@
-import express, { type Express, Response, Request } from "express";
+import express, { type Express } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import fetch from "node-fetch";
+import db from "./utils/db-connection";
+import authRouter from "./routers/auth";
+import userRouter from "./routers/user";
 
 dotenv.config();
 
 const app: Express = express();
 const port = 3001;
-const url = "https://accounts.spotify.com/api/token";
 
-const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
-const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+db.on("error", error => console.error(`Errore connessione al database: \n ${error}`));
+db.once("open", () => console.log("Connessione al database avvenuta con successo"));
 
 app.use(express.json());
 app.use(cors());
-
-app.get("/auth/access-token", (_req: Request, res: Response) => {
-  if (!SPOTIFY_CLIENT_ID || !SPOTIFY_CLIENT_SECRET) {
-    res.status(500).send("CLIENT_ID o CLIENT_SECRET mancanti");
-    return;
-  }
-
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      Authorization: "Basic " + btoa(SPOTIFY_CLIENT_ID + ":" + SPOTIFY_CLIENT_SECRET),
-    },
-    body: new URLSearchParams({
-      grant_type: "client_credentials",
-    }),
-  })
-    .then(response => response.json())
-    .then(data => {
-      res.status(200).send(data);
-    });
-});
+app.use("/auth", authRouter);
+app.use("/user", userRouter);
 
 app.listen(port, () => {
   console.log(`⚡️ Server attivo sulla porta ${port}`);
