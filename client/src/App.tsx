c9a087cbc028a47500d0ba8a3852fd166f99279d
daxdiv <95906740+daxdiv@users.noreply.github.com>
2023-06-23 11:43:14 +0200

    set access token in local storage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,17 @@ import "./styles/globals.css";
 import Button from "./components/ui/Button";
 import CenteredContainer from "./components/ui/CenteredContainer";
 import Input from "./components/ui/Input";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    fetch("http://localhost:3001/auth/access-token")
+      .then(res => res.json())
+      .then(data => {
+        localStorage.setItem("access-token", data.accessToken);
+      });
+  }, []);
+
   return (
     <>
       <CenteredContainer className="mb-2 flex-col gap-2">
