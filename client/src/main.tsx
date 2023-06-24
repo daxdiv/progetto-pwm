78fcd238f461be59fbd6edf0c56a6cb48de799ad
daxdiv <95906740+daxdiv@users.noreply.github.com>
2023-06-24 09:33:20 +0200

    use react-query to get the access token

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,15 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+
 import App from "./App.tsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
