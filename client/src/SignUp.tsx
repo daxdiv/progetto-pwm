b8e218b4c3596e4714dcae4fb895b9ea251e371a
daxdiv <95906740+daxdiv@users.noreply.github.com>
2023-06-24 12:09:32 +0200

    sign up page

--- client/src/SignUp.tsx
+++ client/src/SignUp.tsx
@@ -1,18 +1,75 @@
 import "./styles/globals.css";
 
+import { useRef, useState } from "react";
+
 import Button from "./components/ui/Button";
 import CenteredContainer from "./components/ui/CenteredContainer";
 import Input from "./components/ui/Input";
-import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const usernameRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
+  const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
 
-  //const handleSignUp = async () => {}
+  const handleSignUp = async () => {
+    const username = usernameRef.current?.value;
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+
+    if (!username || !email || !password) {
+      setError("Compila tutti i campi");
+      return;
+    }
+
+    // Fonte: https://www.w3resource.com/javascript/form/email-validation.php
+    // eslint-disable-next-line no-useless-escape
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (!emailRegex.test(email)) {
+      setError("Email non valida");
+      return;
+    }
+
+    const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/auth/sign-up`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      setError(data.message);
+      return;
+    }
+
+    navigate("/?success=Utente registrato correttamente");
+  };
+
   return (
     <>
       <CenteredContainer className="mb-2 flex-col gap-2">
+        {error && (
+          <p className="text-red-500 bg-red-200 px-4 rounded-xl py-1 text-sm mb-1">
+            {error}
+          </p>
+        )}
+        <Input
+          variant="neutral"
+          size="sm"
+          placeholder="Username"
+          type="text"
+          ref={usernameRef}
+        />
         <Input
           variant="neutral"
           size="sm"
@@ -29,9 +86,9 @@ function App() {
         />
         <Button
           type="button"
-          text="Login"
+          text="Registrati"
           className="mt-2"
-          //onClick={handleSignUp}
+          onClick={handleSignUp}
           // className={clsx({ "cursor-not-allowed": isLoading })}
         />
 
