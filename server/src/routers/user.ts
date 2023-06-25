20b2a3ea21d8defa5d14bb82cf122f877f3ad2a5
daxdiv <david200288l@gmail.com>
2023-06-25 20:24:12 +0200

    update user profile api endpoint

--- server/src/routers/user.ts
+++ server/src/routers/user.ts
@@ -28,7 +28,47 @@ router.delete("/:id", async (req: Request, res: Response) => {
 
     res.status(StatusCodes.OK).json(deletedUser); //COMMENT: 200
   } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message }); //COMMENT: 500
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Errore interno, riprovare più tardi" }); //COMMENT: 500
+  }
+});
+
+router.put("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { username, email, password, preferredGenres } = req.body;
+
+  if (!id) {
+    res.status(StatusCodes.BAD_REQUEST).json({ message: "ID utente non fornito" }); //COMMENT: 400
+    return;
+  }
+  if (!isValidObjectId(id)) {
+    res.status(StatusCodes.BAD_REQUEST).json({ message: "ID utente non valido" }); //COMMENT: 400
+    return;
+  }
+
+  if (!username && !email && !password && !preferredGenres) {
+    res.status(StatusCodes.BAD_REQUEST).json({ message: "Nessun campo da modificare" }); //COMMENT: 400
+    return;
+  }
+
+  try {
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: id },
+      { username, email, password, preferredGenres },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      res.status(StatusCodes.NOT_FOUND).json({ message: "Utente non trovato" }); //COMMENT: 404
+      return;
+    }
+
+    res.status(StatusCodes.OK).json(updatedUser); //COMMENT: 200
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Errore interno, riprovare più tardi" }); //COMMENT: 500
   }
 });
 
