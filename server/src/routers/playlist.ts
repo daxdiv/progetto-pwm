461ca81933fcb005d4b48cf8741a1eeb1f218614
daxdiv <david200288l@gmail.com>
2023-06-27 16:44:28 +0200

    delete playlist api endpoint

--- server/src/routers/playlist.ts
+++ server/src/routers/playlist.ts
@@ -1,6 +1,7 @@
 import express, { type Request, Response } from "express";
 import StatusCodes from "http-status-codes";
 import Playlist from "../models/playlist";
+import { isValidObjectId } from "mongoose";
 
 const router = express.Router();
 
@@ -26,6 +27,35 @@ router.post("/", async (req: Request, res: Response) => {
     res.status(StatusCodes.CREATED).json(newPlaylist); //COMMENT: 201
   } catch (error) {
     console.error(error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Errore interno, riprovare più tardi" }); //COMMENT: 500
+  }
+});
+
+router.delete("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  if (!id) {
+    res.status(StatusCodes.BAD_REQUEST).json({ message: "ID playlist non fornito" }); //COMMENT: 400
+    return;
+  }
+
+  if (!isValidObjectId(id)) {
+    res.status(StatusCodes.BAD_REQUEST).json({ message: "ID playlist non valido" }); //COMMENT: 400
+    return;
+  }
+
+  try {
+    const deletedPlaylist = await Playlist.deleteOne({ _id: id });
+
+    if (deletedPlaylist.deletedCount === 0) {
+      res.status(StatusCodes.NOT_FOUND).json({ message: "Playlist non trovata" }); //COMMENT: 404
+      return;
+    }
+
+    res.status(StatusCodes.OK).json(deletedPlaylist); //COMMENT: 200
+  } catch (error) {
     res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .json({ message: "Errore interno, riprovare più tardi" }); //COMMENT: 500
