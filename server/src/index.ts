c2bfbe8fa1a0aeed62cb7ffb532bbb871d7aec40
daxdiv <david200288l@gmail.com>
2023-06-27 11:17:35 +0200

    playlist Model & Router

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import db from "./utils/db-connection";
 import authRouter from "./routers/auth";
 import userRouter from "./routers/user";
+import playlistRouter from "./routers/playlist";
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
+app.use("/playlist", playlistRouter);
 
 app.listen(port, () => {
   console.log(`⚡️ Server attivo sulla porta ${port}`);
---
+++ server/src/routers/playlist.ts
@@ -0,0 +1,28 @@
+import express, { type Request, Response } from "express";
+import StatusCodes from "http-status-codes";
+import Playlist from "../models/playlist";
+
+const router = express.Router();
+
+router.post("/", async (req: Request, res: Response) => {
+  const { userId, title, description, tags, tracks } = req.body;
+
+  if (!userId || !title || !description || !tags || !tracks) {
+    res.status(StatusCodes.BAD_REQUEST).json({ message: "Dati mancanti" }); //COMMENT: 400
+    return;
+  }
+
+  try {
+    const newPlaylist = new Playlist({ userId, title, description, tags, tracks });
+    await newPlaylist.save();
+
+    res.status(StatusCodes.CREATED).json(newPlaylist); //COMMENT: 201
+  } catch (error) {
+    console.error(error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Errore interno, riprovare più tardi" }); //COMMENT: 500
+  }
+});
+
+export default router;
