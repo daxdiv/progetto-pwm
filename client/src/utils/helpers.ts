edcb676bffeab1e7d5643c1aebcd1397740180a3
daxdiv <david200288l@gmail.com>
2023-06-29 12:07:52 +0200

    format duration utility function

--- client/src/utils/helpers.ts
+++ client/src/utils/helpers.ts
@@ -16,3 +16,10 @@ export function formatDate(date: Date) {
 export function truncate(str: string, maxLen: number) {
   return str.length > maxLen ? `${str.slice(0, maxLen - 1)}...` : str;
 }
+
+export function formatDuration(ms: number) {
+  const minutes = Math.floor(ms / 60000);
+  const seconds = ((ms % 60000) / 1000).toFixed(0);
+
+  return `${minutes}:${+seconds < 10 ? "0" : ""}${seconds}`;
+}
