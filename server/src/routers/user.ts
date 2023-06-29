246976488d993b580a6a7db384239b69ff0eac5a
daxdiv <95906740+daxdiv@users.noreply.github.com>
2023-06-29 09:06:49 +0200

    save playlist api endpoint

--- server/src/routers/user.ts
+++ server/src/routers/user.ts
@@ -115,4 +115,59 @@ router.get("/:id/playlists", async (req: Request, res: Response) => {
   }
 });
 
+router.post("/save-playlist", async (req: Request, res: Response) => {
+  const { userId, playlistId } = req.body;
+
+  if (!userId) {
+    res.status(StatusCodes.BAD_REQUEST).json({ message: "ID utente non fornito" }); //COMMENT: 400
+    return;
+  }
+  if (!playlistId) {
+    res.status(StatusCodes.BAD_REQUEST).json({ message: "ID playlist non fornito" }); //COMMENT: 400
+    return;
+  }
+
+  if (!isValidObjectId(userId)) {
+    res.status(StatusCodes.BAD_REQUEST).json({ message: "ID utente non valido" }); //COMMENT: 400
+    return;
+  }
+  if (!isValidObjectId(playlistId)) {
+    res.status(StatusCodes.BAD_REQUEST).json({ message: "ID playlist non valido" }); //COMMENT: 400
+    return;
+  }
+
+  try {
+    const isPlaylistSaved = await User.findOne({
+      _id: userId,
+      savedPlaylists: {
+        $in: [playlistId],
+      },
+    }).count();
+
+    if (isPlaylistSaved > 0) {
+      res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "Hai già salvato questa playlist" }); //COMMENT: 400
+      return;
+    }
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: userId },
+      { $addToSet: { savedPlaylists: playlistId } },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      res.status(StatusCodes.NOT_FOUND).json({ message: "Utente non trovato" }); //COMMENT: 404
+      return;
+    }
+
+    res.status(StatusCodes.OK).json({ message: "Playlist salvata correttamente" }); //COMMENT: 200
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Errore interno, riprovare più tardi" }); //COMMENT: 500
+  }
+});
+
 export default router;
