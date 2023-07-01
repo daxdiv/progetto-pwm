8311609cad1e6724a74dca236fc13a3455e23c2e
daxdiv <95906740+daxdiv@users.noreply.github.com>
2023-07-01 18:59:43 +0200

    handle preferred artist for each user

--- client/src/hooks/useUser.ts
+++ client/src/hooks/useUser.ts
@@ -1,4 +1,5 @@
 import { useQuery, type UseQueryOptions } from "react-query";
+import { useNavigate } from "react-router-dom";
 
 type UserData = {
   id: string;
@@ -6,17 +7,21 @@ type UserData = {
   email: string;
   description: string;
   preferredGenres?: string[];
+  preferredArtists?: string[];
   savedPlaylists?: string[];
 };
 
 export default function useUser(
   userId: string,
-  options?: UseQueryOptions<unknown, Error, UserData>
+  options?: UseQueryOptions<UserData, Error, UserData>
 ) {
+  const navigate = useNavigate();
   const { isLoading, isRefetching } = useQuery({
     queryKey: ["fetch-user-data"],
     queryFn: async () => {
       const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/user/${userId}`);
+
+      if (response.status === 404) navigate("/?profile=Nessun utente trovato");
 
       return (await response.json()) as UserData;
     },
