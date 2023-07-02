a07c518a9a36a928807b604f4c03121cdd3591bd
daxdiv <95906740+daxdiv@users.noreply.github.com>
2023-07-02 17:03:32 +0200

    add swagger-autogen

---
+++ server/src/utils/swagger.ts
@@ -0,0 +1,12 @@
+import swaggerAutogen from "swagger-autogen";
+
+const swaggerDoc = {
+  info: {
+    title: "Progetto finale Programmazione Web e Mobile - Social Network for Music (SNM)",
+    description:
+      "Documentazione delle API del progetto finale di Programmazione Web e Mobile",
+  },
+  host: "localhost:3001",
+};
+
+swaggerAutogen()("./swagger-output.json", "./src/index.ts", swaggerDoc);
