df363e1408fcfbd4decb8a5c4fdb21dac1a601bc
daxdiv <95906740+daxdiv@users.noreply.github.com>
2023-07-03 12:18:07 +0200

    remove swagger explorer

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,11 +17,7 @@ db.on("error", error => console.error(`Errore connessione al database: \n ${erro
 db.once("open", () => console.log("Connessione al database avvenuta con successo"));
 
 app.use(express.json());
-app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerDocument, { explorer: true })
-);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(cors());
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
